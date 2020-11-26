965efc0942692f6ededd7929332857679b4b6914
Victor Guthrie <guthrievictor@gmail.com>
2020-11-26 10:55:53 -0500

    add meal filter

--- src/components/LogFilter.js
+++ src/components/LogFilter.js
@@ -10,6 +10,7 @@ export default ({eventType, selectFilter}) => (
       <Menu.Item active={isNil(eventType)} onClick={selectFilter(null)}>All</Menu.Item>
       <Menu.Item active={isEqual(EVENT.DIAPER, eventType)} onClick={selectFilter(EVENT.DIAPER)}>Diapers</Menu.Item>
       <Menu.Item active={isEqual(EVENT.FEEDING, eventType)} onClick={selectFilter(EVENT.FEEDING)}>Feedings</Menu.Item>
+      <Menu.Item active={isEqual(EVENT.MEAL, eventType)} onClick={selectFilter(EVENT.MEAL)}>Meals</Menu.Item>
       <Menu.Item active={isEqual(EVENT.PUMPING, eventType)} onClick={selectFilter(EVENT.PUMPING)}>Pumpings</Menu.Item>
       <Menu.Item active={isEqual(EVENT.OTHER, eventType)} onClick={selectFilter(EVENT.OTHER)}>Other</Menu.Item>
     </Menu>
